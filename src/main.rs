e9ce5cd4bce17d94e7f073a70f7c05bfed7e9c68
Yap Jia Hong <yapjiahong2003@gmail.com>
2022-10-11 12:22:18 +0800

    Added image data into the binary itself. All good on Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use eframe;
 use pomot::App;
+
+const ICON: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", "pomot.png"));
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-
-    let icon = image::open("pomot.png").expect("Failed to open icon path.").to_rgba8();
+    let icon = image::load_from_memory(ICON)
+    .expect("Failed to open icon path.")
+    .to_rgba8();
     let (icon_width, icon_height) = icon.dimensions();
     let native_options = eframe::NativeOptions {
         icon_data: Some(eframe::IconData {
